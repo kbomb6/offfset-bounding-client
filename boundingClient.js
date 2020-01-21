8b3cd2e77cc64147dc91bfb61e588b5702ae1940
Kelly <31076298+kbomb6@users.noreply.github.com>
2020-01-20 21:36:38 -0700

    Initial Commit

---
+++ boundingClient.js
@@ -0,0 +1,5 @@
+var el = document.getElementsByClassName("testimonialsimage");
+var viewportOffset = el.getBoundingClientRect();
+// these are relative to the viewport, i.e. the window
+var top = viewportOffset.top;
+var left = viewportOffset.left;
